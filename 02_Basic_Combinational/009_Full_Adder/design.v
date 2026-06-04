7ec1a155bbc3d48aecc96966b77c3289cbefc5db
Jason Ranjit <jasonranjit@gmail.com>
2026-06-03 23:14:37 -0500

    full adder design file

---
+++ 02_Basic_Combinational/009_Full_Adder/design.v
@@ -0,0 +1,30 @@
+module half_adder(input a,
+                  input b,
+                  output cout,
+                  output sum);
+  
+  xor(sum,a,b);
+  and(cout,a,b);
+  
+endmodule
+
+module full_adder(input a,
+                  input b,
+                  input cin,
+                  output sum,
+                  output cout);
+
+  wire sum1,cout1,cout2;
+
+  half_adder ha_1(.a(a),
+                  .b(b),
+                  .cout(cout1),
+                  .sum(sum1));
+  half_adder ha_2(.a(sum1),
+                  .b(cin),
+                  .cout(cout2),
+                  .sum(sum));
+
+  or(cout,cout1,cout2);
+
+endmodule
