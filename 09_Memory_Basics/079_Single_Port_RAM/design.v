98cc1e9d02327884743b72721165e02d7ed1361f
Jason Ranjit <jasonranjit@gmail.com>
2026-07-16 00:56:01 -0500

    changes

--- 09_Memory_Basics/079_Single_Port_RAM/design.v
+++ 09_Memory_Basics/079_Single_Port_RAM/design.v
@@ -1,30 +1,18 @@
-module single_port_ram(input [7:0] d_in,
-                       input [4:0] ram_addr,
-                       input w_en,
-                       input clk,
-                       input rst,
-                       output [7:0] d_out
-                      );
+module single_port_ram(
+    input [7:0] d_in,
+    input [4:0] ram_addr,
+    input w_en,
+    input clk,
+    output reg [7:0] d_out
+);
   
   reg [7:0] ram_mem [31:0];
-  reg [4:0] addr_reg;
   
-  always @(posedge clk or posedge rst) begin
-    if(rst) begin
-      for(integer i=0;i<32;i=i+1)
-        ram_mem[i]<=0;
-      addr_reg<=0;
+  always @(posedge clk) begin
+    if (w_en) begin
+      ram_mem[ram_addr] <= d_in;
     end
-    else begin
-      if(w_en)
-        ram_mem[ram_addr] <= d_in;
-      else 
-        addr_reg <= ram_addr;
-    end
+    d_out <= ram_mem[ram_addr]; 
   end
   
-  assign d_out = ram_mem[addr_reg];
-  
 endmodule
-        
-  
--- 09_Memory_Basics/079_Single_Port_RAM/design_tb.v
+++ 09_Memory_Basics/079_Single_Port_RAM/design_tb.v
@@ -1,69 +1,73 @@
+`timescale 1ns/1ps
+
 module single_port_ram_tb();
   reg [7:0] d_in;
   reg [4:0] ram_addr;
-  reg w_en,clk,rst;
-  wire [7:0]d_out;
+  reg w_en, clk;
+  wire [7:0] d_out;
   
-  single_port_ram DUT(.d_in(d_in),
-                      .ram_addr(ram_addr),
-                      .w_en(w_en),
-                      .clk(clk),
-                      .rst(rst),
-                      .d_out(d_out)
-                     );
-  
-  reg [4:0] addr_r;
-  reg [7:0] data_r;
+  single_port_ram DUT (
+    .d_in(d_in),
+    .ram_addr(ram_addr),
+    .w_en(w_en),
+    .clk(clk),
+    .d_out(d_out)
+  );
   
   always #10 clk = ~clk;
   
-  //task for read op and write op in tb
   task write;
     input [4:0] addr;
     input [7:0] data;
     begin
       ram_addr = addr;
-      d_in = data;
-      w_en = 1;
+      d_in     = data;
+      w_en     = 1;
       @(posedge clk);
-      #1
-      w_en = 0;
-      $display("data = %b, memory stored = %b",d_in,ram_addr);
+      #1;
+      w_en     = 0;
+      $display("[WRITE] Addr: %d | Data Written: 0x%h", addr, data);
     end
   endtask
       
-  
   task read;
     input [4:0] addr;
     begin
       ram_addr = addr;
-      w_en = 0;
+      w_en     = 0;
       @(posedge clk);
-      $display("address = %b, Read data = %b",ram_addr,d_out);
+      #1; 
+      $display("[READ]  Addr: %d | Data Read:    0x%h", ram_addr, d_out);
     end
   endtask
   
+  integer i;
+  
   initial begin
+    // Waveform setup
     $dumpfile("image.vcd");
-    $dumpvars(0,single_port_ram_tb.DUT);
+    $dumpvars(0, single_port_ram_tb);
+    
+    // Initialize signals
     clk = 0;
-    rst = 1;
+    w_en = 0;
+    ram_addr = 0;
+    d_in = 0;
+    
     @(posedge clk); 
-    rst = 0;
-    // Write random data to random addresses
-    repeat (20) begin
-      addr_r = $urandom_range(0, 31);
-      data_r = $urandom_range(0, 255);
-      write(addr_r, data_r);
+    
+    $display("--- Write Cycle ---");
+    for (i = 0; i < 10; i = i + 1) begin
+      write(i, $urandom_range(16, 255)); 
     end
 
-    // Read back from random addresses
-    repeat (10) begin
-      addr_r = $urandom_range(0, 31);
-      read(addr_r);
+    $display("\n--- Read Cycle ---");
+    for (i = 0; i < 10; i = i + 1) begin
+      read(i);
     end
     
     @(posedge clk);
+    $display("\nSimulation Finished Successfully.");
     $finish();
   end
 endmodule
